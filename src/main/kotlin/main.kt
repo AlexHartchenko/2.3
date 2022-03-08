e49f0cfe157111c280880b7afe0e88f3d2c28ee0
Alex Hartchenko <aj.tomsk@mail.ru>
2022-03-08 18:28:28 +0700

    доработка 3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,25 @@
 package ru.netology
 
 fun main() {
-    val amount = 1_900
+    val amount = 100_000
     val loyaltyClient = true
     val amountAfterDiscount = amount - calculateInitialDiscount(amount) - calculateLoyalDiscount(amount, loyaltyClient)
     println("Сумма : $amountAfterDiscount руб.")
 }
+
 fun calculateInitialDiscount(amount: Int): Int {
-    val initialDiscount: Int
-    when (amount) {
-        in 0..1_000 -> {
-            initialDiscount = 0
-        }
-        in 1_001..10_000 -> {
-            initialDiscount = 100
-        }
-        else -> {
-            initialDiscount = (amount * 0.05).toInt()
-        }
+    return when (amount) {
+        in 0..1_000 -> 0
+        in 1_001..10_000 -> 100
+        else -> (amount * 0.05).toInt()
     }
-    return initialDiscount
 }
-fun calculateLoyalDiscount(amount : Int, loyal : Boolean) : Int {
-    val discountLoyal: Int
-    if (loyal) {
-        discountLoyal = (amount * 0.01).toInt()
+
+fun calculateLoyalDiscount(amount: Int, loyaltyClient: Boolean): Int {
+    val discountLoyal: Int = if (loyaltyClient) {
+        ((amount - calculateInitialDiscount(amount)) * 0.01).toInt()
     } else {
-        discountLoyal = 0
+        0
     }
     return discountLoyal
 }
